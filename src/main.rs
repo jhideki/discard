b3fd576ab0a9f3132cd6bbd7e09e9fc70d1e9715
jonathan-ami <126116089+jonathan-ami@users.noreply.github.com>
2024-06-28 16:56:38 -0700

    signaler

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,40 @@
+use std::sync::Arc;
+
+use anyhow::Result;
+use lazy_static::lazy_static;
+use reqwest::{Body, Client};
+use tokio::sync::Mutex;
 use webrtc::api::APIBuilder;
+use webrtc::ice_transport::ice_candidate::RTCIceCandidate;
 use webrtc::ice_transport::ice_server::RTCIceServer;
 use webrtc::peer_connection::configuration::RTCConfiguration;
+use webrtc::peer_connection::RTCPeerConnection;
 
+lazy_static! {
+    static ref PEER_CONNECTION_MUTEX: Arc<Mutex<Option<Arc<RTCPeerConnection>>>> =
+        Arc::new(Mutex::new(None));
+}
+
+async fn signal_candidate(addr: &str, c: &RTCIceCandidate) -> Result<()> {
+    let payload = c.to_json()?.candidate;
+    let client = Client::new();
+    let body = Body::from(payload);
+    let _res = match client
+        .post(format!("http://{addr}/candidate"))
+        .header("content-type", "application/json; charset=utf-8")
+        .body(body)
+        .send()
+        .await
+    {
+        Ok(res) => res,
+        Err(e) => {
+            println!("{e}");
+            return Err(e.into());
+        }
+    };
+
+    Ok(())
+}
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let ice_server = RTCIceServer {
