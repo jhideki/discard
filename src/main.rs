f9b6d6c39b904222673bcde3456c984aa458c4ef
jonathan-ami <126116089+jonathan-ami@users.noreply.github.com>
2024-07-02 17:05:10 -0700

    update

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use std::sync::Arc;
 
 use anyhow::Result;
+use http_body_util::combinators::BoxBody;
+use http_body_util::BodyExt;
+use hyper::body::Bytes;
+use hyper::{Method, Request, Response};
+use hyper_util::rt::TokioIo;
 use lazy_static::lazy_static;
-use reqwest::{Body, Client};
+use tokio::net::TcpStream;
 use tokio::sync::Mutex;
 use webrtc::api::APIBuilder;
 use webrtc::ice_transport::ice_candidate::RTCIceCandidate;
@@ -13,27 +18,45 @@ use webrtc::peer_connection::RTCPeerConnection;
 lazy_static! {
     static ref PEER_CONNECTION_MUTEX: Arc<Mutex<Option<Arc<RTCPeerConnection>>>> =
         Arc::new(Mutex::new(None));
+    static ref PENDING_CANDIDATES: Arc<Mutex<Vec<RTCIceCandidate>>> =
+        Arc::new(Mutex::new(Vec::new()));
+    static ref ADRESS: Arc<Mutex<String>> = Arc::new(Mutex::new(String::new()));
 }
 
 async fn signal_candidate(addr: &str, c: &RTCIceCandidate) -> Result<()> {
     let payload = c.to_json()?.candidate;
-    let client = Client::new();
-    let body = Body::from(payload);
-    let _res = match client
-        .post(format!("http://{addr}/candidate"))
-        .header("content-type", "application/json; charset=utf-8")
-        .body(body)
-        .send()
-        .await
-    {
-        Ok(res) => res,
-        Err(e) => {
-            println!("{e}");
-            return Err(e.into());
-        }
-    };
+    let req = Request::builder()
+        .method(Method::POST)
+        .uri(format!("http://{addr}/candidate"))
+        .header("content-type", "application/josn; charset=utf-8")
+        .body(Bytes::from(payload));
+    let stream = TcpStream::connect(addr).await?;
+    let io = TokioIo::new(stream);
+    let (mut sender, conn) = hyper::client::conn::http1::handshake(io).await?;
+    let _res = sender.send_request(req).await?;
 
     Ok(())
+}
+async fn remote_handler(
+    req: Request<hyper::body::Incoming>,
+) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
+    let pc = {
+        let pcm = PEER_CONNECTION_MUTEX.lock().await;
+        pcm.clone().unwrap()
+    };
+    let addr = {
+        let addr = ADRESS.lock().await;
+        addr.clone()
+    };
+
+    match (req.method(), req.uri().path()) {
+        (&Method::POST, "/candidate") => {
+            let cand = match std::str::from_utf8(&req.collect().await?.to_bytes()) {
+                Ok(s) => s,
+                Err(e) => panic!("{e}"),
+            };
+        }
+    }
 }
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
