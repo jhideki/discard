2099aec444582e0e6d6fbf7950b483f1c04f6f14
jonathan-ami <126116089+jonathan-ami@users.noreply.github.com>
2024-07-04 17:01:15 -0700

    updates lol

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
+mod body;
+
 use std::sync::Arc;
 
 use anyhow::Result;
 use http_body_util::combinators::BoxBody;
-use http_body_util::BodyExt;
-use hyper::body::Bytes;
-use hyper::{Method, Request, Response};
+use http_body_util::{BodyExt, Empty, Full};
+use hyper::body::{Body, Bytes, Frame};
+use hyper::{Method, Request, Response, StatusCode};
 use hyper_util::rt::TokioIo;
 use lazy_static::lazy_static;
 use tokio::net::TcpStream;
 use tokio::sync::Mutex;
 use webrtc::api::APIBuilder;
-use webrtc::ice_transport::ice_candidate::RTCIceCandidate;
+use webrtc::ice_transport::ice_candidate::{RTCIceCandidate, RTCIceCandidateInit};
 use webrtc::ice_transport::ice_server::RTCIceServer;
 use webrtc::peer_connection::configuration::RTCConfiguration;
+use webrtc::peer_connection::sdp::session_description::RTCSessionDescription;
 use webrtc::peer_connection::RTCPeerConnection;
 
 lazy_static! {
@@ -23,13 +26,13 @@ lazy_static! {
     static ref ADRESS: Arc<Mutex<String>> = Arc::new(Mutex::new(String::new()));
 }
 
-async fn signal_candidate(addr: &str, c: &RTCIceCandidate) -> Result<()> {
+async fn signal_candidate<B>(addr: &str, c: &RTCIceCandidate) -> Result<()> {
     let payload = c.to_json()?.candidate;
     let req = Request::builder()
         .method(Method::POST)
         .uri(format!("http://{addr}/candidate"))
-        .header("content-type", "application/josn; charset=utf-8")
-        .body(Bytes::from(payload));
+        .header("content-type", "application/json; charset=utf-8")
+        .body(Body)?;
     let stream = TcpStream::connect(addr).await?;
     let io = TokioIo::new(stream);
     let (mut sender, conn) = hyper::client::conn::http1::handshake(io).await?;
@@ -37,6 +40,7 @@ async fn signal_candidate(addr: &str, c: &RTCIceCandidate) -> Result<()> {
 
     Ok(())
 }
+
 async fn remote_handler(
     req: Request<hyper::body::Incoming>,
 ) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
@@ -51,12 +55,92 @@ async fn remote_handler(
 
     match (req.method(), req.uri().path()) {
         (&Method::POST, "/candidate") => {
-            let cand = match std::str::from_utf8(&req.collect().await?.to_bytes()) {
+            let candidate = match std::str::from_utf8(&req.collect().await?.to_bytes()) {
+                Ok(s) => s.to_owned(),
+                Err(e) => panic!("{e}"),
+            };
+            if let Err(e) = pc
+                .add_ice_candidate(RTCIceCandidateInit {
+                    candidate,
+                    ..Default::default()
+                })
+                .await
+            {
+                panic!("{e}");
+            }
+            let mut response = Response::new(empty());
+            *response.status_mut() = StatusCode::OK;
+            Ok(response)
+        }
+        (&Method::POST, "/sdp") => {
+            let sdp_str = match std::str::from_utf8(&req.collect().await?.to_bytes()) {
+                Ok(s) => s.to_owned(),
+                Err(e) => panic!("{e}"),
+            };
+
+            let sdp = match serde_json::from_str::<RTCSessionDescription>(&sdp_str) {
                 Ok(s) => s,
                 Err(e) => panic!("{e}"),
             };
+
+            let answer = match pc.create_answer(None).await {
+                Ok(a) => a,
+                Err(e) => panic!("{e}"),
+            };
+
+            let payload = match serde_json::to_string(&answer) {
+                Ok(p) => p,
+                Err(e) => panic!("{e}"),
+            };
+
+            let req = Request::builder()
+                .method(Method::POST)
+                .uri(format!("http://{addr}/sdp"))
+                .header("content-type", "application/josn; charset=utf-8")
+                .body(Bytes::from(payload));
+
+            let stream = match TcpStream::connect(addr).await {
+                Ok(s) => s,
+                Err(e) => panic!("{e}"),
+            };
+
+            let io = TokioIo::new(stream);
+
+            let (mut sender, conn) = match hyper::client::conn::http1::handshake(io).await {
+                Ok((s, c)) => (s, c),
+                Err(e) => panic!("{e}"),
+            };
+
+            let _res = match sender.send_request(req).await {
+                Ok(res) => res,
+                Err(e) => {
+                    println!("{e}");
+                    return Err(e);
+                }
+            };
+
+            if let Err(e) = pc.set_local_description(answer).await {
+                panic!("{e}");
+            }
+
+            {
+                let cs = PENDING_CANDIDATES.lock().await;
+                for c in &*cs {
+                    if let Err(e) = signal_candidate(&addr, c).await {
+                        panic!("{e}");
+                    }
+                }
+            }
+            let mut response = Response::new(empty());
+            *response.status_mut() = StatusCode::OK;
+            Ok(response)
         }
     }
+}
+fn empty() -> BoxBody<Bytes, hyper::Error> {
+    Empty::<Bytes>::new()
+        .map_err(|never| match never {})
+        .boxed()
 }
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
