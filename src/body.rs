125039cec93eac0adb48c92c0ec99fef5af39ad7
jhideki <126116089+jhideki@users.noreply.github.com>
2024-07-16 17:03:37 -0700

    updates

--- src/body.rs
+++ src/body.rs
@@ -1,15 +1,2 @@
-use hyper::body::{Body, Buf, Bytes};
-use std::convert::Infallible;
-pub struct MessageBody {
-    data: Vec<u8>,
-    position: usize,
-}
-impl Body for MessageBody {
-    type Data = Buf;
-    type Error = Infallible;
-    fn poll_frame(
-        self: std::pin::Pin<&mut Self>,
-        cx: &mut std::task::Context<'_>,
-    ) -> std::task::Poll<Option<Result<hyper::body::Frame<Self::Data>, Self::Error>>> {
-    }
-}
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-mod body;
-
 use std::sync::Arc;
 
 use anyhow::Result;
 use http_body_util::combinators::BoxBody;
-use http_body_util::{BodyExt, Empty, Full};
-use hyper::body::{Body, Bytes, Frame};
+use http_body_util::{BodyExt, Empty};
+use hyper::body::Bytes;
+use hyper::service::service_fn;
 use hyper::{Method, Request, Response, StatusCode};
 use hyper_util::rt::TokioIo;
 use lazy_static::lazy_static;
@@ -23,19 +22,24 @@ lazy_static! {
         Arc::new(Mutex::new(None));
     static ref PENDING_CANDIDATES: Arc<Mutex<Vec<RTCIceCandidate>>> =
         Arc::new(Mutex::new(Vec::new()));
-    static ref ADRESS: Arc<Mutex<String>> = Arc::new(Mutex::new(String::new()));
+    static ref ADDRESS: Arc<Mutex<String>> = Arc::new(Mutex::new(String::new()));
 }
 
-async fn signal_candidate<B>(addr: &str, c: &RTCIceCandidate) -> Result<()> {
+async fn signal_candidate(addr: &str, c: &RTCIceCandidate) -> Result<()> {
     let payload = c.to_json()?.candidate;
-    let req = Request::builder()
+    let req = match Request::builder()
         .method(Method::POST)
         .uri(format!("http://{addr}/candidate"))
         .header("content-type", "application/json; charset=utf-8")
-        .body(Body)?;
+        .body(BoxBody::new(payload))
+    {
+        Ok(req) => req,
+        Err(e) => panic!("Failed to build request {}", e),
+    };
+
     let stream = TcpStream::connect(addr).await?;
     let io = TokioIo::new(stream);
-    let (mut sender, conn) = hyper::client::conn::http1::handshake(io).await?;
+    let (mut sender, _conn) = hyper::client::conn::http1::handshake(io).await?;
     let _res = sender.send_request(req).await?;
 
     Ok(())
@@ -49,7 +53,7 @@ async fn remote_handler(
         pcm.clone().unwrap()
     };
     let addr = {
-        let addr = ADRESS.lock().await;
+        let addr = ADDRESS.lock().await;
         addr.clone()
     };
 
@@ -93,13 +97,17 @@ async fn remote_handler(
                 Err(e) => panic!("{e}"),
             };
 
-            let req = Request::builder()
+            let req = match Request::builder()
                 .method(Method::POST)
                 .uri(format!("http://{addr}/sdp"))
                 .header("content-type", "application/josn; charset=utf-8")
-                .body(Bytes::from(payload));
+                .body(BoxBody::new(payload))
+            {
+                Ok(req) => req,
+                Err(e) => panic!("Request failed: {}", e),
+            };
 
-            let stream = match TcpStream::connect(addr).await {
+            let stream = match TcpStream::connect(&addr).await {
                 Ok(s) => s,
                 Err(e) => panic!("{e}"),
             };
@@ -135,37 +143,73 @@ async fn remote_handler(
             *response.status_mut() = StatusCode::OK;
             Ok(response)
         }
+        _ => {
+            let mut not_found = Response::default();
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            Ok(not_found)
+        }
     }
 }
+
 fn empty() -> BoxBody<Bytes, hyper::Error> {
     Empty::<Bytes>::new()
         .map_err(|never| match never {})
         .boxed()
 }
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let ice_server = RTCIceServer {
-        urls: vec!["stun:stun.l.google.com:19302".to_owned()],
-        ..Default::default()
-    };
+async fn main() -> Result<()> {
+    let answer_addr = String::from("localhost:60000");
+    let offer_addr = String::from("0.0.0:50000");
+    {
+        let mut oa = ADDRESS.lock().await;
+        oa.clone_from(&answer_addr);
+    }
+
     let config = RTCConfiguration {
-        ice_servers: vec![ice_server],
+        ice_servers: vec![RTCIceServer {
+            urls: vec!["stun:stun.l.google.come:19302".to_owned()],
+            ..Default::default()
+        }],
         ..Default::default()
     };
     let api = APIBuilder::new().build();
-    let peer_connection = api.new_peer_connection(config).await?;
+    let peer_connection = Arc::new(api.new_peer_connection(config).await?);
+    let pc = Arc::downgrade(&peer_connection);
+    let pending_candidates2 = Arc::clone(&PENDING_CANDIDATES);
+    let addr2 = answer_addr.clone();
 
-    let offer = peer_connection.create_offer(None).await?;
-    peer_connection.set_local_description(offer.clone()).await?;
-
-    peer_connection
-        .on_ice_candidate(Box::new(|candidate| {
-            Box::pin(async move {
-                if let Some(candidate) = candidate {
-                    println!("ICE Candidate: {:?}", candidate.candidate);
+    peer_connection.on_ice_candidate(Boxx::new(move |c: Option<RTCIceCandidate>| {
+        println!("on ice candidate {:?}", c);
+        let pc2 = pc.clone();
+        let pending_candidates3 = Arc::clone(&pending_candidates2);
+        let addr3 = addr2.clone();
+        Box::pin(async move {
+            if let Some(c) = c {
+                if let Some(pc) = pc2.upgrade() {
+                    let desc = pc.remote_description().await;
+                    if desc.is_none() {
+                        let mut cs = pending_candidates3.lock().await;
+                        cs.push(c);
+                    } else if let Err(e) = signal_candidate(&addr3, &c).await {
+                        panic!("{}", e);
+                    }
                 }
-            })
-        }))
-        .await;
-    Ok(())
+            }
+        })
+    }));
+
+    println!("Listening on http://{offer_addr}");
+    {
+        let mut pcm = PEER_CONNECTION_MUTEX.lock().await;
+        *pcm = Some(Arc::clone(&peer_connection));
+    }
+
+    tokio::spawn(async move {
+        let addr = SocketAddr::from_str(&offer_addr).unwrap();
+        let service = service_fn(||)
+        let server = Server::bind(&addr).servce(service);
+        if let Err(e) = server.await {
+            eprintln!("Server error: {e}");
+        }
+    })
 }
